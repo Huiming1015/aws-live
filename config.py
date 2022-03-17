47f07c88da0f87d9c83307e50d041cefc42d804b
Huiming1015 <61903443+Huiming1015@users.noreply.github.com>
2022-03-17 15:39:28 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cpkgr6uadfgx.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "simhuiming-employee"
+customregion = "us-east-1"
